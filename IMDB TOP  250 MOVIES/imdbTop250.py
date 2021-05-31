cf4e5bd98bec739c155a4a8517875da6eaf825bc
PhurbaGyalzen <sphurba861@gmail.com>
2021-05-31 16:02:42 +0545

    movie scraper

---
+++ IMDB TOP  250 MOVIES/imdbTop250.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+url = 'https://www.imdb.com/chart/top/'
+s = requests.Session()
+r = s.get(url)
+
+soup = BeautifulSoup(r.text, 'lxml')
+
+movies = soup.find('table', class_='chart full-width').find('tbody').find_all('tr')
+position = 0
+MovieDetails = []
+for movie in movies:
+    position += 1
+    title = movie.find('td', class_='titleColumn').find('a').text.strip()
+    year = movie.find('td', class_='titleColumn').find('span').text.strip()[1:5]
+    rating = movie.find('td', class_='ratingColumn imdbRating').text.strip()
+    
+    detail = {
+        'postition': position,
+        'title': title,
+        'year': year,
+        'rating': rating,
+    }
+
+    MovieDetails.append(detail)
+
+with open('imdbTop250.json', 'w') as f:
+    json.dump(MovieDetails, f)
+
+print('Complete!!!')
